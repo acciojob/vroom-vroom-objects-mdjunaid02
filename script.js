df5dabe123e65c3f305cdc67533b5eef6ba4fc30
mdjunaid02 <156643108+mdjunaid02@users.noreply.github.com>
2025-10-16 12:16:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	constructor(make, model){
+		this.make=make;
+		this.model=model;
+	}
+	getMakeModel(){
+		console.log(`${this.make} ${this.model}`);
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar extends Car(make, model, topSpeed) {
+	constructor(make,model,topSpeed){
+		this.make=make;
+		this.model=model;
+		this.topSpeed=topSpeed;
+	}
+	getTopSpeed(){
+		console.log(`${topSpeed}`)
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
